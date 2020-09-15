1da4345ae8178e1af311b6bb5796b6681a949c95
peaky76 <robertjamespeacock@gmail.com>
2020-09-15 20:14:00 +0100

    basic components and imports set up

---
+++ src/components/Song.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Song = (props) => {
+  return <p>THIS IS A SONG</p>;
+};
+
+export default Song;
---
+++ src/containers/SongsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Song from "../components/Song";
+
+class SongsContainer extends Component {
+  render() {
+    return (
+      <>
+        <div>THIS CONTAINS SONGS</div>
+        <Song />
+      </>
+    );
+  }
+}
+
+export default SongsContainer;
