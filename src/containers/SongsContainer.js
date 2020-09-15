1c52e4106d7f6f3382bd5952a87a03d966de4320
peaky76 <robertjamespeacock@gmail.com>
2020-09-15 20:20:15 +0100

    fetch working

--- src/containers/SongsContainer.js
+++ src/containers/SongsContainer.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import Song from "../components/Song";
 
 class SongsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      songs: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }
+
   render() {
     return (
       <>
